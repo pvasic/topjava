45ea7210131827dd6c948f29ddcbd8188d5bf94a
Pavel Vasichkin <pl.vasichkin@gmail.com>
2021-03-03 09:01:18 +0300

    move spring-inmemory.xml to test folder

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -14,7 +14,7 @@ import static ru.javawebinar.topjava.UserTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({"classpath:spring/spring-app.xml",
-        "classpath:spring/spring-inmemory.xml"})
+        "file:src/test/resources/spring/spring-inmemory.xml"})
 @RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
 
